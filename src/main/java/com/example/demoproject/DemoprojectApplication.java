3934c44e93db68de7b00719db8fd3f86b4f1f27f
Samuel C Moore <48695356+samuelmoorec@users.noreply.github.com>
2020-06-25 15:38:32 -0500

    first commit

---
+++ src/main/java/com/example/demoproject/DemoprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoprojectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demoproject/DemoprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
